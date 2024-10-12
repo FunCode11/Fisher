5d260a7f7868c43442ddc430c251152b0d46e59e
Tamin Rahman <96876308+FunCode11@users.noreply.github.com>
2024-10-12 19:13:21 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Require the necessary discord.js classes
-const fs = require('node:fs');
-const path = require('node:path');
+const fs = require('fs');
+const path = require('path');
 const { Client, Events, GatewayIntentBits, Collection } = require('discord.js');
 const { token } = require('./config.json');
 
