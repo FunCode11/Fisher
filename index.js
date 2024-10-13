d3e53fac3e8b62542cc20c2712823ac6b74b81fa
Tamin Rahman <96876308+FunCode11@users.noreply.github.com>
2024-10-13 09:12:32 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const fs = require('fs');
 const path = require('path');
 const { Client, Events, GatewayIntentBits, Collection } = require('discord.js');
-const { token } = require('./config.json');
+const token = process.env.TOKEN
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
