d70f5601968a33440cf9290c1ec7d7c05ebc33af
Tamin Rahman <96876308+FunCode11@users.noreply.github.com>
2024-10-13 09:39:24 -0400

    Update fish.js

--- commands/utility/fish.js
+++ commands/utility/fish.js
@@ -1,11 +1,11 @@
 const { SlashCommandBuilder } = require('discord.js');
 const math = require('mathjs');
-
+const fishes = ["tuna", "sardine", "hering", "catfish", "krill", "tilapia", "carp"]
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('fish')
 		.setDescription('Fishes!'),
 	async execute(interaction) {
-		await interaction.reply("You fished " + math.floor(math.random(0, 2183)) + " fishes!");
+		await interaction.reply("You fished a " + fishes[math.randomInt(6)] + "!");
 	},
 };
